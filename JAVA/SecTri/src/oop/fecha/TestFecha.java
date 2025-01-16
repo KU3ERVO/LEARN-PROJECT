e9494f0d1e15c7a73b7a2a84ef9f1d1a0f2e3a1c
KUERVO <shino.off.mail@gmail.com>
2025-01-16 13:42:55 +0100

    updt

--- JAVA/SecTri/src/oop/fecha/TestFecha.java
+++ JAVA/SecTri/src/oop/fecha/TestFecha.java
@@ -1,13 +1,15 @@
 package oop.fecha;
-import java.time.LocalDate;
+import java.util.Date;
 public class TestFecha {
 
 	public static void main(String[] args) {
 		
+		Date ld = new Date();
 
 		Fecha f1 = new Fecha(31, 12, 2000);
 		
 		System.out.println(f1.toString());
+		System.out.println(ld);
 
 	}
 
