7cd27e74aa1cd6058e65333a743657dccaebd11c
KUERVO <shino.off.mail@gmail.com>
2025-01-27 13:50:54 +0100

    p

--- DB/OnlineGamesDB.sql
+++ DB/OnlineGamesDB.sql
@@ -1,5 +1,11 @@
 CREATE DATABASE OnlineGames;
 SHOW DATABASES;
 CREATE TABLE USERS(
-id INT(8) primary key
+user_id INT8 primary key,
+user_name VARCHAR (255),
+email VARCHAR (255),
+nick CHAR (10),
+login TEXT,
+password TEXT,
+birthdate TEXT,
 );
