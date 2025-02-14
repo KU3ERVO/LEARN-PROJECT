8af8237de679ed6ad9f64d1f3f30ac785f8424d4
KUERVO <shino.off.mail@gmail.com>
2025-02-14 13:39:22 +0100

    new almacen

---
+++ JAVA/SecTri/src/oop/almacen/Producto.java
@@ -0,0 +1,13 @@
+package oop.almacen;
+/*PRODUCTOS DE ALIMENTACION:
+ * ELECTRONICA PLUS GARANTIA
+ * ALIMENTACION FECHA CADUCIDAD
+ * ROPA MATERIAL
+ * ARRAYLIST VENTA
+ * Utilizar producto[i] instanceof Ropa          ((Ropa)producto[i].getMaterial())
+ * */
+public class Producto {
+	
+	
+
+}
