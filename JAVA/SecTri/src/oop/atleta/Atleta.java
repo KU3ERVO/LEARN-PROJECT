fc5bdbc851ae641c62e9bba9e11f67cb95b42bf0
KUERVO <shino.off.mail@gmail.com>
2025-02-24 11:26:12 +0100

    new almacen

--- JAVA/SecTri/src/oop/atleta/Atleta.java
+++ JAVA/SecTri/src/oop/atleta/Atleta.java
@@ -1,7 +1,13 @@
 package oop.atleta;
-
+/**
+ * @author ANGEL
+ * @version 1.0.0
+ * **/
 public class Atleta {
 	
+	private static String seleccion;
+	private static int contador;
+	private static double tiempoEquipo;
 	
 
 }
