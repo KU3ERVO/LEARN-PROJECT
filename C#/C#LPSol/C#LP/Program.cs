372f0685c4c1644e15dc30bb2998b64b6ab7f608
KU3ERVO <shino.off.mail@gmail.com>
2025-03-29 02:05:11 +0100

    C#3

--- C#/C#LPSol/C#LP/Program.cs
+++ C#/C#LPSol/C#LP/Program.cs
@@ -2,6 +2,30 @@
 
 class Program
 {
+
+    int Add(int a, int b)
+    {
+
+        return a + b;
+
+    }
+
+    int Add(int a, int b, int c)
+    {
+
+        return a + b + c;
+
+    }
+
+    double Add(double a, double b)
+    {
+
+        return a * b;
+
+    }
+
+
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello world");
@@ -12,5 +36,31 @@ class Program
         bool d = bool.Parse(Console.ReadLine());
 
         Console.WriteLine(a + b + c + d);
+
+        Console.WriteLine($"wha ha hape {a} to lady jane {b} {c}");
+
+        while (a < 10)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(c);
+            }
+            a++;
+        }
+
+        List<int> list = new List<int>();
+        
+        list.Add(a);
+        list.Add(5);
+
+        foreach (int i in list) {
+            
+            Console.WriteLine("\n"+i);
+        
+        }
+    
+    
     }
+      
 }
