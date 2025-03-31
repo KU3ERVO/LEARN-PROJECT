d5be204b7fb9ec6f34b2293a208dc4aa58f9fcb0
KUERVO <shino.off.mail@gmail.com>
2025-03-31 13:39:29 +0200

    java mystring

--- JAVA/ThirdTri/src/figuras/FiguraTest.java
+++ JAVA/ThirdTri/src/figuras/FiguraTest.java
@@ -3,12 +3,9 @@ package figuras;
 public class FiguraTest {
 
 	public static void main(String[] args) {
-		
-		String h1 = "hola";
-		String h2 = "hola";
-		
-		System.out.println(h1.compareTo(h2));
 
+	
+		
 	}
 
 }
--- JAVA/ThirdTri/src/myString/MyString.java
+++ JAVA/ThirdTri/src/myString/MyString.java
@@ -2,4 +2,109 @@ package myString;
 
 public class MyString {
 
+	private char[] cadena;
+	private String mystring;
+
+	public MyString(String cadena)
+	{
+
+	}
+
+	public MyString(char[] cadena)
+	{        
+
+		String mystring = "";
+
+		for(char c : cadena) {
+
+			mystring += c;
+
+		}
+
+	}
+
+	public MyString(ArrayList<Character> cadena)
+	{
+	}
+
+	public char[] getCadena()
+	{
+
+	}
+
+	public char myCharAt(int i) throws Exception
+	{
+	}
+
+	public int myCompareTo(MyString cadena2)
+	{
+	}
+
+	public int myCompareToIgnoreCase(MyString cadena2)
+	{
+	}  
+
+	public void myConcat(MyString cadena2)
+	{   
+	}
+
+	public int myIndexOf(int caracter)
+	{
+	}
+
+	public int myLastIndexOf(int caracter)
+	{
+	}
+
+	public int myLength()
+	{
+
+	}
+
+	public MyString myReplace(char antiguo, char nuevo)
+	{
+	}
+
+	/*
+	 * public MyString[] mySplit(char expresion)
+	    {
+	    }
+	 */
+
+	public MyString[] mySplit(char expresion)
+	{
+	}
+
+
+
+	public MyString mySubstring(int inicio, int fin) throws Exception
+	{
+	}
+
+	public MyString myToLowerCase()
+	{
+	}
+
+	public MyString myToLowerCase(char[] cadena)
+	{
+
+	}
+
+	public MyString myToUpperCase()
+	{
+	}
+
+	public MyString myTrim()
+	{
+	}
+
+	public void imprimir()
+	{
+	}
+
+	public String myToString()
+	{
+
+	}
+
 }
