672fd708587e6c928e769a0bae4b56941955899f
KU3ERVO <shino.off.mail@gmail.com>
2025-11-13 13:23:39 +0100

    java JDBC ND MOBILE

--- JAVA/ControlesBasicos/ControlesBasicos/app/src/main/java/ejercicios/Ej54.java
+++ JAVA/ControlesBasicos/ControlesBasicos/app/src/main/java/ejercicios/Ej54.java
@@ -1,6 +1,7 @@
 package ejercicios;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,12 @@ public class Ej54 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_5_4);
 
+        EditText edtxtTelefono = findViewById(R.id.editTextPhone);
+        EditText edtxtCorreo = findViewById(R.id.editTextEmail);
+        EditText edtxtNumero = findViewById(R.id.editTextNumber);
+        EditText edtxtContraseña = findViewById(R.id.editTextNumberPassword);
+
+edtxtTelefono.setInputType();
 
     }
 }
