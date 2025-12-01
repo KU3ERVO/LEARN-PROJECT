9b4130a9d865dad930e1ff66bc6ceaaca3d1e87c
KU3ERVO <shino.off.mail@gmail.com>
2025-12-01 13:48:36 +0100

    javamobile

--- JAVA/ListadosYMenus/app/src/main/java/com/example/listadosymenus/ejercicios/Ej61.java
+++ JAVA/ListadosYMenus/app/src/main/java/com/example/listadosymenus/ejercicios/Ej61.java
@@ -1,4 +1,15 @@
 package com.example.listadosymenus.ejercicios;
 
-public class Ej61 {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Ej61 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
